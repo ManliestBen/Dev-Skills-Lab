e79b1e29ab56c535354ecb60f2e99a345e1fdcd4
ManliestBen <benjamintmanley@gmail.com>
2019-08-13 21:15:35 -0500

    using index and show properly

---
+++ models/skill.js
@@ -0,0 +1,21 @@
+const skills = [
+    {skill: 'Learn HTML', learned: true},
+    {skill: 'Learn CSS', learned: true},
+    {skill: 'Learn JavaScript', learned: true},
+    {skill: 'Feed the hungry squid < % == ', learned: false},
+    {skill: 'Add confetti', learned: true}
+  ];
+  
+  module.exports = {
+    getAll,
+    getOne
+  };
+  
+  function getAll() {
+    return skills;
+  }
+
+  function getOne(id) {
+    return skill[id];
+  }
+
